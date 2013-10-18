047f51d8fa42b798c56ee82c04be770ce742c670
rajav <rajav@c07bd4d5-e112-473b-942e-3cc843f1f005>
2013-10-18 22:20:44 +0000

    SmartApps Server: Added a diagnostics page to check status of eJabber
    - Added a diagnostics page to check the status of eJabberD (the XMPP
      server) and RabbitMQ (which is used to execute tasks asynchronously).
    - The code also has logic to redirect to a 404 error page (this is
      currently commented out).
    - The Diagnostics page is at http://neatostaging.rajatogo.com/diagnostics/
    - NOTE: A future check-in will improve the UI. The time specified is in GMT.

---
+++ trunk/Server_Yii/Neato/diagnostics/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Neato Diagnostics Services</title>
+<style>
+.success{
+	color:blue; font-weight:bold;font-size:20px; 
+}
+.error{
+	color:red; font-weight:bold;font-size:20px; 
+}
+div.last-ran-at{
+	color:black; font-size:12px; font-style:italic;
+}
+div.information{
+	color:black; font-size:14px; font-style:italic;
+	padding-bottom:10px;
+}
+</style>
+</head>
+<body>
+<h1>
+<a href="index.php">Neato Diagnostics Services</a>
+</h1>
+<div class="information">Ejabberd and RabbitMQ are critical for the Neato App's Server end. <br/> Using this page, you can easily find out the status of Ejabberd and RabbitMQ services. <br/>Click on the links below to see the status of these services.</div>
+<table>
+<tr><td><a href="ejabberd_status.php">Ejabberd Status</a></td></tr>
+<tr><td><a href="rabbitmq_status.php">RabbitMQ Status</a></td></tr>
+</table>
+</body>
+</html>
